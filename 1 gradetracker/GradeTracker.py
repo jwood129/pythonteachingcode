ac72c7b260c5d61a09920c12afee067953c65a42
jwood129 <jwood129@students.kennesaw.edu>
2022-09-12 20:28:23 -0400

    Update GradeTracker.py

--- 1 gradetracker/GradeTracker.py
+++ 1 gradetracker/GradeTracker.py
@@ -5,46 +5,67 @@ needs to have login to protect the student data
 '''
 #import libraries first
 import statistics as s
+from xml.dom import WRONG_DOCUMENT_ERR
 
 #add constants next
-admins = {'Faculty1':'ABC123','Faculty2':'ABC123'}
+admins = {'Faculty1':'ABC123','Faculty2':'ABC123','Jada': 'Wood'}
 
-#Like the admins above is a dictionary but of students. Dictionaries use curly brackets with colons to associate keys with values. In this case, each student's first name is a key. The values are lists of grades. 
-#Lists are denoted with square brackets. Values are indexed within starting with 0 for the first one. Each value is separated by commas.
+#Like the admins above is a dictionary but of students. 
+#Dictionaries use curly brackets with colons to associate keys with values. 
+#In this case, each student's first name is a key. The values are lists of grades. 
+#Lists are denoted with square brackets.
+#Values are indexed within starting with 0 for the first one.
+#Each value is separated by commas.
 students = {'Alex':[87,88,98],
             'Sally':[88,67,93],
             'Nboke':[90,88,78]}
 
-#Now we define functions. Functions encapsulate logic into reusable recipes that can be executed whenever we need them by calling their name with parentheses.
-def enterGrades():
-    nameToEnter = input('Student name: ')
-    gradeToEnter = input('Grade: ')
-    #This checks through the keys of the students dictionary to see if the name entered exactly matches any one in there.
-    if nameToEnter in students:
-        print('Adding grade for'+nameToEnter)
-        students[nameToEnter].append(float(gradeToEnter)) #float will have a .0
-        print(str(nameToEnter)+' now has these grades:')
-        print(students[nameToEnter])
-    else:
+#Now we define functions. Functions encapsulate logic into reusable recipes that can be 
+#executed whenever we need them by calling their name with parentheses.
+def enter_grades():
+ '''
+ Function to input a grade for a given student. 
+ '''
+name_to_enter = input('Student name: ')
+grade_to_enter = input('Grade: ')
+    #This checks through the keys of the students dictionary to see if the name entered 
+    # exactly matches any one in there.
+if name_to_enter in students:
+        print('Adding grade for'+name_to_enter)
+        students[name_to_enter].append(float(grade_to_enter)) #float will have a .0
+        print(str(name_to_enter)+' now has these grades:')
+        print(students[name_to_enter])
+else:
         print('Student not found. Please check your spelling or go back and add if new.')
 
-def removeStudent():
-    nameToRemove = input('Who do you want to remove? ')
-    if nameToRemove in students:
-        print('Removing '+nameToRemove)
-        del students[nameToRemove]
+def remove_student():
+     '''
+     Function to remove a specific student.
+      '''
+     name_to_remove = input('Who do you want to remove? ')
+     if name_to_remove in students:
+        print('Removing '+name_to_remove)
+        del students[name_to_remove]
         print(students)
-    else:
+     else:
         print('Student not found.')
 
-def averageStudents():
-    for student in students:
+def average_students():
+     '''
+      Funtion to average all students' grades.
+      '''
+     for student in students:
         grades = students[student]
-        average = s.mean(grades) #notice the s? we imported the statistuics module as s. Thus, we are using a fucntion called "mean()" from the statistics module.
+        average = s.mean(grades) #notice the s? we imported the statistuics module as s.
+        # Thus, we are using a fucntion called "mean()" from the statistics module.
         print(student,' average ',average)
 
 def main():
-    print("User: " + login)
+  '''
+  Function to show user main menu and process option selections.
+  '''
+
+  print("User: " + login)
     #Here we present our main menu options once a person logs in successfully.
     print("""
     Welcome to the Grade Tracker
@@ -59,22 +80,24 @@ def main():
     #Here we process their choice of what they want to do.
     if action == '1':
         #print('1 selected')
-        enterGrades()
+        
+        ()
     elif action == '2':
         #print('2 selected')
-        removeStudent()
+        remove_student()
     elif action == '3':
         #print('3 selected')
-        averageStudents()
-    elif action == '4':
+        #average_students()
+        # elif action == '4':
         #print('4 selected')
         exit()
     else:
         print('Valid option not selected.') #need to cause it to reprompt
 
-login = input('User: ')
 
-if login in admins:
+  login = input('Faculty account name: ')
+
+  if login in admins:
     password = input('Password: ')
     if admins[login] == password:
         print('Welcome,',login)
@@ -83,5 +106,6 @@ if login in admins:
             main()
     else:
         print('Invalid password.')
-else:
+  else:
     print('Invalid user.')
+
